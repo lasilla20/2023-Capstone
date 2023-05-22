465cc92354b9d55e12f43f6931207c415c7d07ab
aiming1 <seulah0629@gmail.com>
2023-05-22 16:25:06 +0900

    테스트 완료

---
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+/** 깃허브 테스트용 주석 (편집 시 삭제 바람) **/
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
