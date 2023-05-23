6f915931728c7c9b9eb2c5a88a07ea2a2402f66b
Jeong Chami <lasilla20@naver.com>
2023-05-23 14:34:46 +0900

    testing...

--- src/main/java/com/example/project/ProjectApplication.java
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectApplication {
-/** 깃허브 테스트용 주석
-(편집 시 삭제 바람!!) **/
+/** 깃허브 테스트용 주석 (편집 시 삭제 바람!!) **/
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
