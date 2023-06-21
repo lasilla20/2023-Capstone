b29dfe3dc89e45514fbed368ab99102ba8f6551d
Kim SeulA <seulah0629@gmail.com>
2023-06-21 18:22:02 +0900

    DB 자동 연결 삭제 (DB 아직 안 정함...)

--- src/main/java/com/example/project/ProjectApplication.java
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ProjectApplication {
 /** 깃허브 테스트용 주석 (편집 시 삭제 바람!!) **/
+/** DB 정해지면 application.properties 수정 후 위 annotation의 exclude 옵션 삭제 **/
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
