9b090556dd1431833de5f38dd4ed22c4e1758589
Kim SeulA <seulah0629@gmail.com>
2023-07-03 00:41:53 +0900

    차미

--- src/main/java/com/example/project/Controller/HelloWorldController.java
+++ src/main/java/com/example/project/Controller/HelloWorldController.java
@@ -34,4 +34,16 @@ public class HelloWorldController {
         return "private";
     }
 
+    /* UserController 에 존재
+    @GetMapping("/loginForm")
+    public String login(){
+        return "login";
+    }
+
+    MainController 에 존재
+    @GetMapping("/main")
+    public String main(){
+        return "main";
+    }
+    */
 }
