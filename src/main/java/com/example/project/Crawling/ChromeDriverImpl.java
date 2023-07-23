535c31858ce1d4e79009463dcf06bf77f79b4aa5
hyunmin0317 <choihm9903@naver.com>
2023-07-23 11:29:56 +0900

    fix: chromedriver path 설정

--- src/main/java/com/example/project/Crawling/ChromeDriverImpl.java
+++ src/main/java/com/example/project/Crawling/ChromeDriverImpl.java
@@ -4,13 +4,19 @@ import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
 @Component
 public class ChromeDriverImpl implements ChromeDriver{
 
+    private static String WEB_DRIVER_PATH = "/Users/hyunmin/Desktop/2023-Capstone/chromedriver_macos";
     private WebDriver webDriver;
 
     public ChromeDriverImpl() {
+        if (ObjectUtils.isEmpty(System.getProperty("webdriver.chrome.driver"))) {
+            System.setProperty("webdriver.chrome.driver", WEB_DRIVER_PATH);
+        }
+
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         chromeOptions.addArguments("--remote-allow-origins=*");
