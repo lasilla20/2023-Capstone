8b3062c099c870a47c852dd30ae3c52020dcf683
Kim SeulA <seulah0629@gmail.com>
2023-07-26 17:49:39 +0900

    리액트+스프링

--- src/main/frontend/src/components/Layout/Itemlist.js
+++ src/main/frontend/src/components/Layout/Itemlist.js
@@ -1,25 +1,53 @@
-import React from "react";
-import { ListItem } from "../ListItem";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import styles from "../../styles/css/ItemList.module.css";
-import dummy from "../../db/data.json";
+import {ListItem} from "../ListItem";
 
 const ItemList = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const fetchData = async () => {
+    try {
+      // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+      setError(null);
+      setData(null);
+      // loading 상태를 true 로 바꿉니다.
+      setLoading(true);
+      const response = await axios.get(
+          '/api'
+      );
+      setData(response.data); // 데이터는 response.data 안에 들어있습니다.
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!data) return null;
   return (
-    <div className={styles.itemlistcontent}>
-      {dummy.products.map((item) => (
-        <ListItem
-          className={styles.listItem}
-          key={item.id}
-          id={item.id}
-          store={item.store}
-          price={item.price}
-          title={item.title}
-          src={item.thumbnail}
-          heartCnt={item.heartCnt}
-        />
-      ))}
-    </div>
+      <div className={styles.itemlistcontent}>
+        {data.map((item) => (
+            <ListItem
+                className={styles.listItem}
+                key={item.name}
+                id={item.id}
+                store={item.market}
+                price={item.price}
+                title={item.name}
+                src={item.image}
+                heartCnt={item.heartCnt}
+            />
+        ))}
+      </div>
   );
-};
+}
 
 export default ItemList;
